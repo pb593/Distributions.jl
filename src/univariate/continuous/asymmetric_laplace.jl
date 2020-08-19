f1be0e0a52423a739d30b00d563e20b48f9d82d1
Pavel Berkovich <pavel.berkovich@invenialabs.co.uk>
2020-08-19 15:14:21 +0300

    Added expressions for skewness, kurtosis and entropy of ALD.

--- src/univariate/continuous/asymmetric_laplace.jl
+++ src/univariate/continuous/asymmetric_laplace.jl
@@ -8,6 +8,8 @@ end
 
 mean(d::AsymmetricLaplace) = d.μ + (1 - d.κ^2) / (d.θ * d.κ)
 var(d::AsymmetricLaplace) = (1 + d.κ^4) / (d.θ * d.κ)^2
+skewness(d::AsymmetricLaplace) = 2 * (1 - d.κ^6) / (d.κ^4 + 1)^(3/2)
+kurtosis(d::AsymmetricLaplace) = 6 * (1 + d.κ^8) / (1 +  d.κ^4)^2
 median(d::AsymmetricLaplace) =
     d.μ +
     if (d.κ >= 1)
@@ -16,6 +18,7 @@ median(d::AsymmetricLaplace) =
         - log((1 + d.κ^2) / 2) / (d.θ * d.κ)
     end
 
+entropy(d::AsymmetricLaplace) = 1 + log((1 + d.κ^2) / (d.κ * d.θ))
 params(d::AsymmetricLaplace) = (d.μ, d.θ, d.κ)
 
 rand(rng::AbstractRNG, d::AsymmetricLaplace) =
